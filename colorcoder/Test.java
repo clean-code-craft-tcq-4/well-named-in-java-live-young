52b84491ce8da6789b993023c8a58e64978ab325
live-young <61771639+live-young@users.noreply.github.com>
2022-06-15 22:56:52 +0530

    Add files via upload

---
+++ colorcoder/Test.java
@@ -0,0 +1,31 @@
+package colorcoder;
+public class Test {
+	   static void testNumberToPair(int pairNumber,
+		        MajorColor expectedMajor,
+		        MinorColor expectedMinor)
+		    {
+		        ColorPair colorPair = Main.getColorFromPairNumber(pairNumber);
+		        System.out.println("Got pair " + colorPair.convertToString());
+		        assert(colorPair.getMajor() == expectedMajor);
+		        assert(colorPair.getMinor() == expectedMinor);
+		    }
+
+		    static void testPairToNumber(
+		        MajorColor major,
+		        MinorColor minor,
+		        int expectedPairNumber)
+		    {
+		        int pairNumber = Main.getPairNumberFromColor(major, minor);
+		        System.out.println("Got pair number " + pairNumber);
+		        assert(pairNumber == expectedPairNumber);
+		    }
+		    
+			public void testColorCode() {
+				testNumberToPair(4, MajorColor.WHITE, MinorColor.BROWN);
+				testNumberToPair(5, MajorColor.WHITE, MinorColor.SLATE);
+
+				testPairToNumber(MajorColor.BLACK, MinorColor.ORANGE, 12);
+				testPairToNumber(MajorColor.VIOLET, MinorColor.SLATE, 25);
+			}
+
+}
