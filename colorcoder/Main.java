29c6caf30e4b9b3d190c6d71782f5b4fe6828696
live-young <61771639+live-young@users.noreply.github.com>
2022-06-15 22:57:41 +0530

    Add files via upload

---
+++ colorcoder/Main.java
@@ -0,0 +1,35 @@
+package colorcoder;
+
+public class Main {
+
+	static ColorPair getColorFromPairNumber(int pairNumber) {
+		int zeroBasedPairNumber = pairNumber - 1;
+		MajorColor majorColor = MajorColor.fromIndex(zeroBasedPairNumber / ColorPair.NUMBEROFMAJORCOLORS);
+		MinorColor minorColor = MinorColor.fromIndex(zeroBasedPairNumber % ColorPair.NUMBEROFMINORCOLORS);
+		return new ColorPair(majorColor, minorColor);
+	}
+
+	static int getPairNumberFromColor(MajorColor major, MinorColor minor) {
+		return major.getIndex() * ColorPair.NUMBEROFMINORCOLORS + minor.getIndex() + 1;
+	}
+	
+	static void displayManual() {
+        int count = 1;
+        System.out.println("Pair No." + " -> " + "Major Color" + " - " + "Minor Color");
+        for(int i = 0 ; i< ColorPair.MAJORCOLORNAMES.length ; i++) {
+            for(int j = 0; j<ColorPair.MINORCOLORNAMES.length ; j++) {
+                 System.out.println(count + " -> " + ColorPair.MAJORCOLORNAMES[i] + " -- " + ColorPair.MINORCOLORNAMES[j]);
+                 count++;
+            }
+        }
+    }
+
+	public static void main(String[] args) {
+		Test.testNumberToPair(4, MajorColor.WHITE, MinorColor.BROWN);
+		Test.testNumberToPair(5, MajorColor.WHITE, MinorColor.SLATE);
+
+		Test.testPairToNumber(MajorColor.BLACK, MinorColor.ORANGE, 12);
+		Test.testPairToNumber(MajorColor.VIOLET, MinorColor.SLATE, 25);
+		displayManual();
+	}
+}
